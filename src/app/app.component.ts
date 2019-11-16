179e4239d5c5bf1970f7baf6b31ef57490d51073
sulco <tomek.sulkowski@gmail.com>
2019-11-16 16:24:46 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { kebabCase } from 'lodash'
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = kebabCase('Angular Hello Training');
 }
